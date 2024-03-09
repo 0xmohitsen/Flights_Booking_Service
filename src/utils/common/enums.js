3fd5c63898d61ce098d9ee4675bc96e86dab1e18
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-09 11:23:30 +0530

    Added enum for booking status

--- src/utils/common/enums.js
+++ src/utils/common/enums.js
@@ -5,6 +5,14 @@ const SEAT_TYPE = {
     FIRST_CLASS: 'first-class'
 }
 
+const BOOKING_STATUS = {
+    BOOKED: 'booked',
+    CANCELLED: 'cancelled',
+    INITIATED: 'initiated',
+    PENDING: 'pending'
+}
+
 module.exports = {
-    SEAT_TYPE
+    SEAT_TYPE,
+    BOOKING_STATUS
 }
