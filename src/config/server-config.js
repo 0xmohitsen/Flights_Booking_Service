cddcc92baba60f13ab4197b80b6968226cecb82f
Mohitsen11 <mohitsen0102@gmail.com>
2024-04-08 22:21:24 +0530

    Removed the extra queue file and modified server-config

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,9 +6,5 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     FLIGHT_SERVICE: process.env.FLIGHT_SERVICE,
-    MSG_QUEUE: process.env.MSG_QUEUE,
-    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
-    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
-    QUEUE_NAME: process.env.QUEUE_NAME,
-    FLIGHT_NOTIFICATION_SERVICE: process.env.FLIGHT_NOTIFICATION_SERVICE
+    MSG_QUEUE: process.env.MSG_QUEUE
 }
